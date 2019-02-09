861068e7c154995a33e2a417761b35d18342c120
Katlyyn <magikatlin@gmail.com>
2019-02-09 18:26:54 +0200

    Update main.js

--- hello-world/src/main.js
+++ hello-world/src/main.js
@@ -4,9 +4,3 @@ var app = new Vue({
     message: 'Greetings from Spring Boot!'
   }
 })
-var app2 = new Vue({
-  el: '#app-2',
-  data: {
-    message: 'You loaded this page on ' + new Date().toLocaleString()
-  }
-})
