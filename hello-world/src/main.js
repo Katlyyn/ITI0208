bdf9098824ac2eba7b6506c877837303c020de22
Katlyyn <magikatlin@gmail.com>
2019-02-09 18:24:04 +0200

    Add files via upload

--- hello-world/src/main.js
+++ hello-world/src/main.js
@@ -4,3 +4,9 @@ var app = new Vue({
     message: 'Greetings from Spring Boot!'
   }
 })
+var app2 = new Vue({
+  el: '#app-2',
+  data: {
+    message: 'You loaded this page on ' + new Date().toLocaleString()
+  }
+})
